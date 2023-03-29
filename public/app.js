f77ac290fc24966df1b8ec0948009d0be31c66b5
Tomohiko Takahashi <takahashi@tomohiko.io>
2023-03-30 00:15:12 +0900

    add js

---
+++ public/app.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const ctx = document.getElementById('awsCertificationsChart').getContext('2d');
+    const chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['高橋', '春日', '本田'],
+            datasets: [
+                {
+                    label: 'Practitioner',
+                    data: [0, 1, 1],
+                    backgroundColor: 'rgba(102, 102, 102, 0.6)',
+                    borderColor: 'rgba(102, 102, 102, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Associate',
+                    data: [3, 2, 1],
+                    backgroundColor: 'rgba(0, 153, 204, 0.6)',
+                    borderColor: 'rgba(0, 153, 204, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Professional',
+                    data: [2, 2, 1],
+                    backgroundColor: 'rgba(255, 153, 0, 0.6)',
+                    borderColor: 'rgba(255, 153, 0, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Speciality',
+                    data: [1, 1, 0],
+                    backgroundColor: 'rgba(153, 0, 255, 0.6)',
+                    borderColor: 'rgba(153, 0, 255, 1)',
+                    borderWidth: 1
+                },
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    stacked: true,
+                    title: {
+                        display: true,
+                        text: 'Number of Certifications'
+                    }
+                },
+                x: {
+                    stacked: true,
+                }
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'AWS Certifications by Employee'
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            },
+            indexAxis: 'x',
+            stacked: true
+        }
+    });
+});

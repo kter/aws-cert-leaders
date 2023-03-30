094e52c2359939f62031e8f02091eef38012e76b
Tomohiko Takahashi <takahashi@tomohiko.io>
2023-03-31 08:33:54 +0900

    add test

--- test/leaderboard.test.ts
+++ test/leaderboard.test.ts
@@ -1,17 +1,37 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Leaderboard from '../lib/leaderboard-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as Leaderboard from '../lib/leaderboard-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/leaderboard-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Leaderboard.LeaderboardStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+import { App } from 'aws-cdk-lib';
+import { LeaderboardStack } from '../lib/leaderboard-stack';
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test('should create a Github Actions OIDC role', () => {
+  const app = new App();
+  const stack = new LeaderboardStack(app, 'TestStack');
+  const template = Template.fromStack(stack);
+
+  // Expect that a GithubActionsRole resource is created with the correct properties
+  template.hasResourceProperties('AWS::IAM::Role', {
+    AssumeRolePolicyDocument: {
+      Statement: [
+        {
+          Action: 'sts:AssumeRoleWithWebIdentity',
+          Effect: 'Allow',
+          Condition: {
+            StringLike: {
+                "token.actions.githubusercontent.com:sub": "repo:kter/aws-cert-leaders:*"
+              },
+              StringEquals: {
+                "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+              }
+          }
+        }
+      ],
+      Version: '2012-10-17'
+    },
+    ManagedPolicyArns: [
+      { 'Fn::Join': ['', ['arn:', { Ref: 'AWS::Partition' }, ':iam::aws:policy/AdministratorAccess']] }
+    ],
+    RoleName: 'aws-cert-leaders-deploy-role'
+  });
 });
